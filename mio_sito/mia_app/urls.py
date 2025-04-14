50fc62b9836af3c1497568f3e239e96093bf981e
LauraCippy <lauracomparelli912@gmail.com>
2025-04-14 15:56:20 +0200

    Esperimento con sqlite finito con successo, continuo tutorial fine parte 3 e inizio parte 4

--- mio_sito/mia_app/urls.py
+++ mio_sito/mia_app/urls.py
@@ -4,13 +4,12 @@ from django.conf.urls.static import static
 
 from . import views
 
+app_name = "mia_app"
 urlpatterns = [
-    path("", views.index, name="index"),
-                  # ex: /mia_app/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-                  # ex: /mia_app/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-                  # ex: /mia_app/5/vote/
+
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mio_sito/mia_app/views.py
+++ mio_sito/mia_app/views.py
@@ -1,20 +1,40 @@
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
 
-def index(request):
-    question_list = Question.objects.order_by("domanda")
-    context = {"question_list": question_list}
-    return render(request, "mia_app/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "mia_app/index.html"
+    context_object_name = "question_list"
+    def get_queryset(self):
+        return Question.objects.order_by("domanda")
 
-def detail(request, question_id):
-    domanda = get_object_or_404(Question, pk=question_id)
-    return render(request, "mia_app/detail.html", {"question": domanda})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "mia_app/detail.html"
 
-def results(request, question_id):
-    response = "Stai vedendo il risultato della domanda: %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "mia_app/results.html"
 
 def vote(request, question_id):
-    return HttpResponse("stai votando la domanda %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "mia_app/detail.html",
+            {
+                "question": question,
+                "error_message": "Non hai scelto una risposta",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("mia_app:results", args=(question.id,)))
